0512c42bf2caa467071c2832a71dd9d61a70db7e
truongvan <truongvan@live.com>
2019-03-08 10:42:08 +0700

    add database heroku

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -3,4 +3,6 @@ import django_heroku
 from .base import *
 
 DEBUG = False
+
+DATABASES['default'] = dj_database_url.config(os.environ['DATABASE_URL'], conn_max_age=600)
 django_heroku.settings(locals())
