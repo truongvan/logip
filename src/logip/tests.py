eb64e738b565de3cc6d8c386b4ee2057d65d1db6
truongvan <truongvan@live.com>
2019-03-08 11:37:28 +0700

    add testcase to logip

--- src/logip/tests.py
+++ src/logip/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import Client, TestCase
+
+
+class NoDataTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_no_data_request(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+class UpdateRecordTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def update_record(self):
+        response = self.client.get('/update/')
+        self.assertEqual(response.status_code, 200)
+
+    def view_record(self):
+        response = self.client.get('/')
+        response_dict = json.loads(response)
+        self.assertEqual(response_dict.ip, '127.0.0.1')
